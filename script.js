278a08da1e4311320f8ff1709c2e7b1a8e88faa7
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2026-04-10 10:49:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ async function incarcaDate() {
     const tableBody = document.getElementById('table-body');
     const lastUpdate = document.getElementById('last-update');
     
-    const response = await fetch('date_meciuri.json?t=' + new Date().getTime());
-    const data = await response.json();
+    try {
+        const response = await fetch('date_meciuri.json?t=' + new Date().getTime());
+        const data = await response.json();
 
-    lastUpdate.innerText = data.ultima_actualizare;
-    tableBody.innerHTML = '';
+        lastUpdate.innerText = data.ultima_actualizare;
+        tableBody.innerHTML = '';
 
-    data.meciuri.forEach(meci => {
-        let rowClass = meci.is_value ? 'value-row' : '';
-        tableBody.innerHTML += `
-            <tr class="${rowClass}">
-                <td><strong>${meci.echipa_gazda} - ${meci.echipa_oaspete}</strong></td>
-                <td>1</td>
-                <td>${meci.cota_reala}</td>
-                <td><strong>${meci.cota_1}</strong></td>
-                <td>Livescore</td>
-                <td>${meci.value_procent}%</td>
-            </tr>
-        `;
-    });
+        data.meciuri.forEach(meci => {
+            let rowClass = meci.is_value ? 'value-row' : '';
+            tableBody.innerHTML += `
+                <tr class="${rowClass}">
+                    <td><strong>${meci.echipa_gazda} - ${meci.echipa_oaspete}</strong></td>
+                    <td>1</td>
+                    <td>${meci.cota_reala}</td>
+                    <td><strong>${meci.cota_1}</strong></td>
+                    <td>Pinnacle/Unibet</td>
+                    <td>${meci.value_procent}%</td>
+                </tr>
+            `;
+        });
+    } catch (error) {
+        console.error("Eroare:", error);
+    }
 }
 window.onload = incarcaDate;
