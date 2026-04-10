bb7b171724899aea56e589dc3f5ac8390ce32d47
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2026-04-10 11:31:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,21 @@ async function incarcaDate() {
         lastUpdate.innerText = data.ultima_actualizare;
         tableBody.innerHTML = '';
 
-        data.meciuri.forEach(meci => {
-            let rowClass = meci.is_value ? 'value-row' : '';
-            tableBody.innerHTML += `
-                <tr class="${rowClass}">
-                    <td><strong>${meci.echipa_gazda} - ${meci.echipa_oaspete}</strong></td>
-                    <td>1</td>
-                    <td>${meci.cota_reala}</td>
-                    <td><strong>${meci.cota_1}</strong></td>
-                    <td>Unibet</td>
-                    <td>${meci.value_procent}%</td>
-                </tr>
-            `;
-        });
+data.meciuri.forEach(meci => {
+    let rowClass = meci.is_value ? 'value-row' : '';
+    let valueColor = meci.value_procent > 5 ? 'value-high' : 'value-medium';
+    
+    tableBody.innerHTML += `
+        <tr class="${rowClass}">
+            <td><strong>${meci.echipa_gazda} - ${meci.echipa_oaspete}</strong></td>
+            <td>1</td>
+            <td>${meci.cota_reala}</td>
+            <td><strong>${meci.cota_1}</strong></td>
+            <td>Unibet</td>
+            <td class="${valueColor}">${meci.value_procent}%</td>
+        </tr>
+    `;
+});
     } catch (error) {
         console.error("Eroare:", error);
     }
